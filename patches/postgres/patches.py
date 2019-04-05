3f31da2c6016e4861b9d7f67202f5c0411b87972
Amirhossein Saemi <saemiamirhossein@gmail.com>
2019-04-05 15:19:58 -0400

    improved performance of patched settings

--- patches/postgres/patches.py
+++ patches/postgres/patches.py
@@ -2,35 +2,35 @@
 
 """START OF PATCH"""
 
-import os
-import json
+from json import loads
+from os import environ
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = environ.get('SECRET_KEY')
 # Debug Info
 # never use DEBUG=True in production.
-DEBUG_ENVVAR = os.environ.get('DEBUG', '')
+DEBUG_ENVVAR = environ.get('DEBUG', '')
 # because an envvar is just a string and each string is considered True
 # in python we have to determine if the boolean is True or False
 DEBUG = False
 if DEBUG_ENVVAR.lower() == "true":
     DEBUG = True
 
-ALLOWED_HOSTS = json.loads(os.environ.get('ALLOWED_HOSTS'))
+ALLOWED_HOSTS = loads(environ.get('ALLOWED_HOSTS'))
 
 # DB configurations
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': environ.get('DB_NAME'),
+        'USER': environ.get('DB_USER'),
+        'PASSWORD': environ.get('DB_PASSWORD'),
+        'HOST': environ.get('DB_HOST'),
+        'PORT': environ.get('DB_PORT'),
     }
 }
 
-STATIC_URL = os.environ.get('STATIC_URL')
-MEDIA_URL = os.environ.get('MEDIA_URL')
+STATIC_URL = environ.get('STATIC_URL')
+MEDIA_URL = environ.get('MEDIA_URL')
 
 STATIC_ROOT = "/static/"
 MEDIA_ROOT = '/media/'
